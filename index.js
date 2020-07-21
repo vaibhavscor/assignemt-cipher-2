666359fbb30bc0acb23c0dd191b1bf65d7fbd142
vaibhav <vaibhav.scor29@gmail.com>
2020-07-21 23:52:12 +0530

    assigment 2 js questions

---
+++ index.js
@@ -0,0 +1,30 @@
+
+// Q1. Convert Minutes into Seconds
+var convert(no) {
+    console.log(no*60);
+};
+
+// Q2. Divides Evenly
+
+var dividesEvenly (nemo,dnemo){
+    if(nemo%dnemo == 0){
+        return true;
+    }
+};
+
+                                 // (Easy) 
+
+//  Q1. Count Instances of a Character in a String
+
+
+// Q2. Add up the Numbers from a Single Number
+
+
+
+// Q3. Replace Vowel with Another Character
+
+
+
+
+
+                                  // (Medium)
